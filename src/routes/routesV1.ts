24da1c61db65a8d1a6b8957bac1503138cfd5c45
Alex Xavier <alexjr.123@hotmail.com>
2023-12-06 22:01:51 -0300

    Adiciona estrutura inicial do banco de dados

--- src/routes/routesV1.ts
+++ src/routes/routesV1.ts
@@ -2,14 +2,15 @@ import express from "express";
 
 const routesV1 = express.Router();
 
+routesV1.post("/login", (req, res) => {
+  res.send("Realiza Login.");
+});
+
 routesV1.post("/users", (req, res) => {
   res.send("Cadastra um usuário.");
 });
 routesV1.get("/users", (req, res) => {
   res.send("Lista todos os usuários.");
-});
-routesV1.post("/users/login", (req, res) => {
-  res.send("Realiza Login.");
 });
 routesV1.get("/users/:id", (req, res) => {
   res.send("Lista usuário por id.");
